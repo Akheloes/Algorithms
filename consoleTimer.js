4dbdd527593f8aa4c4ab50fdbe30449cce4efa4b
unknown <fayssaloudbib@gmail.com>
2020-03-17 22:07:06 +0100

    consoleTimer function which time-mesures the performance of a function

--- consoleTimer.js
+++ consoleTimer.js
@@ -1,3 +1,9 @@
 /**
- * @todo: a function which takes a callback and performance-times its execution
+ * A function which takes a a function to time 'timedFunction' and performance-times its execution.
+ * All arguments of the 'timedFunction'
  */
+function consoleTimer(label, timedFunction, ...args) {
+    console.time(label);
+    timedFunction(args);
+    console.timeEnd(label);
+}
