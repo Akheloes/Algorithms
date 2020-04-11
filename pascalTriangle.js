e0997bdb2a359ae3fbda9a0d77d94d1d81edba32
unknown <fayssaloudbib@gmail.com>
2020-04-11 12:07:20 +0200

    LUT binormial function, optimization of pascal's triangle naive approach

--- pascalTriangle.js
+++ pascalTriangle.js
@@ -14,4 +14,40 @@ function factorial(i) {
     return f;
 }
 
-console.log(pascalTriangle(100, 17));
+/**
+ * A LUT (look-up table) algorithm is less computation intensive, has much better performance
+ */
+var binomials = [
+    [1],
+    [1,1],
+    [1,2,1],
+    [1,3,3,1],
+    [1,4,6,4,1],
+    [1,5,10,10,5,1],
+    [1,6,15,20,15,6,1],
+    [1,7,21,35,35,21,7,1]
+]; // max_n = 7
+// [1,8,28,56,70,56,28,8,1]
+
+
+function binomial(n, k) {
+    // return binomials[n][k];
+    if (n < binomials.length) { return binomials[n][k];}
+    if (n >= binomials.length) {
+        let size = binomials.length - 1;
+        while (size <= n) {
+            let array = [1];
+            for(let i=1, s = size; i<=s; i++) {
+                array[i] = binomials[s][i-1] + (binomials[s][i] == undefined ? 0 : binomials[s][i]);
+            }
+            array[size + 1] = 1
+            binomials.push(array);
+            size++;
+        }
+        return binomials[n];
+    }
+}
+
+// console.log(binomial(9, 1));
+
+// console.log(pascalTriangle(100, 17));
