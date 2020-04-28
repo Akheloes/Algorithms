508f39d34fe8bc6fe3741a546e693a8553819bb9
unknown <fayssaloudbib@gmail.com>
2020-04-28 20:41:25 +0200

    commonSubstrings, only not inter-contained substrings

--- commonSubstrings.js
+++ commonSubstrings.js
@@ -1,4 +1,34 @@
 /**
+ * Takes an array an returns only the elements which are not intercontained,
+ * for example: 'ab' and 'a' are intercontained, 'abce' and 'bc' are intercontained, etc.
+ * 'abc' and 'bd' are not intercontained
+ */
+function intercontained(firstString, secondString) {
+    return (firstString.includes(secondString) || secondString.includes(firstString))
+}
+
+/**
+ * Takes an array and return it's intercontained-purified-version, 
+ * that is, if two elements are intercontained, they the smallest is eliminated.
+ * Example: if input is ['ee', 'abcd', 'ab', 'abc', 'e', 'abcd', 'abcde'], output is ['ee', 'abcde']
+ */
+function interContainelessArray(array) {
+    let purified = [];
+    for(let outer=0; outer < array.length; outer++) {
+        let biggest = undefined;
+        for(let inner=0; inner < array.length; inner++) {
+            if(intercontained(array[outer], array[inner]) && outer !== inner) {
+                biggest = array[outer].length > array[inner].length ? array[outer] : array[inner];
+            }
+        }
+        if(biggest && !purified.includes(biggest)) {
+            purified.push(biggest);
+        }
+    }
+    return purified;
+}
+
+/**
  * Returns the array of commun substrings of two strings
  */
 // @TODO: eliminate revisiting the substrings of a substring
@@ -23,15 +53,12 @@ function commonSubstrings(firstString, secondString) {
                 if(substring.length > 1) {
                     substringsArray.push(substring);
                 }
-                // i = _i_;
-                // k = _k_;
             }
         }
     }
-    return substringsArray
-    .filter((item, index, array) => {return array.indexOf(item) === index})
+    let prefiltered = substringsArray.filter((item, index, array) => {return array.indexOf(item) === index});
+    return interContainelessArray(prefiltered);
 }
-
 
 
 /**
