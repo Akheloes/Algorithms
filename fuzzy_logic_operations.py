0ac7373a123e28d6e745e9a3f18bb6a1c5c80215
Akheloes <fayssaloudbib@gmail.com>
2020-07-07 20:16:11 +0200

    trying out the intersection (fuzzy_and) function

--- fuzzy_logic_operations.py
+++ fuzzy_logic_operations.py
@@ -5,19 +5,23 @@ if __name__ == "__main__":
     X = np.linspace(start=0, stop=75, num=75, endpoint=True, retstep=False)
     # Creating membership functions
     kidsMembershipTriangularFunction = [0, 5, 10] #Membership function for child-age (childhood)
+    teenagersMembershipTrangularFunction = [10, 15, 20] #Membership function for teenagers
     youthMembershipTriangularFunction = [15, 25, 30] #Membership function for youth
     middleAgedMembershipTriangularFunction = [35, 40, 45] #Membership function for middle-aged
 
     childhoodDegree = fuzz.membership.trimf(X, kidsMembershipTriangularFunction) #The degree of belonging of X-members to child's category
-    younthDegree = fuzz.membership.trimf(X, youthMembershipTriangularFunction) #The degree of belonging of X-members to youth's category
+    teenagersDegree = fuzz.membership.trimf(X, teenagersMembershipTrangularFunction)
+    youthDegree = fuzz.membership.trimf(X, youthMembershipTriangularFunction) #The degree of belonging of X-members to youth's category
     middleAgedDegree = fuzz.membership.trimf(X, middleAgedMembershipTriangularFunction) #The degree of belonging of X-members to middle-aged's category
 
     # print('____________Array of ages____________')
     # print(X)
     # print('____________kids membership degrees____________')
     # print(childhoodDegree)
-    # print('____________youth membership degrees____________')
-    # print(younthDegree)
+    print('____________teenagers membership degrees____________')
+    print(teenagersDegree)
+    print('____________youth membership degrees____________')
+    print(youthDegree)
     # print('____________middle-aged membership degrees____________')
     # print(middleAgedDegree)
 
@@ -30,7 +34,15 @@ if __name__ == "__main__":
     # print(zero)
 
     # https://pythonhosted.org/scikit-fuzzy/api/skfuzzy.html#fuzzy-not
-    union = fuzz.fuzzy_or(X, younthDegree, X, middleAgedDegree)
-    print('_________________Union__________________')
-    print(union[1])
+    # union = fuzz.fuzzy_or(X, youthDegree, X, middleAgedDegree)[1]
+    # print('_________________Union__________________')
+    # print(union)
+
+    # https://pythonhosted.org/scikit-fuzzy/api/skfuzzy.html#fuzzy-and
+    intersection = fuzz.fuzzy_and(X, teenagersDegree, X, youthDegree)[1]
+    print('_________________Intersection__________________')
+    print(intersection)
+
+
+
 
