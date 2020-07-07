44fd5d6c5601b93d2a6e9c07f8fb85494cd6f555
Akheloes <fayssaloudbib@gmail.com>
2020-07-07 19:56:33 +0200

    trying out union function

--- fuzzy_logic_operations.py
+++ fuzzy_logic_operations.py
@@ -12,11 +12,25 @@ if __name__ == "__main__":
     younthDegree = fuzz.membership.trimf(X, youthMembershipTriangularFunction) #The degree of belonging of X-members to youth's category
     middleAgedDegree = fuzz.membership.trimf(X, middleAgedMembershipTriangularFunction) #The degree of belonging of X-members to middle-aged's category
 
-    print('____________Array of ages____________')
-    print(X)
-    print('____________kids membership degrees____________')
-    print(childhoodDegree)
-    print('____________youth membership degrees____________')
-    print(younthDegree)
-    print('____________middle-aged membership degrees____________')
-    print(middleAgedDegree)
+    # print('____________Array of ages____________')
+    # print(X)
+    # print('____________kids membership degrees____________')
+    # print(childhoodDegree)
+    # print('____________youth membership degrees____________')
+    # print(younthDegree)
+    # print('____________middle-aged membership degrees____________')
+    # print(middleAgedDegree)
+
+    one = np.ones(75)
+    zero = np.zeros((75,))
+
+    # print('_________________Ones__________________')
+    # print(one)
+    # print('_________________Zeros__________________')
+    # print(zero)
+
+    # https://pythonhosted.org/scikit-fuzzy/api/skfuzzy.html#fuzzy-not
+    union = fuzz.fuzzy_or(X, younthDegree, X, middleAgedDegree)
+    print('_________________Union__________________')
+    print(union[1])
+
