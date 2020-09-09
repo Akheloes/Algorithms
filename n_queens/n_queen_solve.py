44c72434025a3b7b5efb57e21b25864495e3700b
Akheloes <fayssaloudbib@gmail.com>
2020-09-09 18:27:51 +0200

    study 3 on solving the n_queen algorithm

--- n_queens/n_queen_solve.py
+++ n_queens/n_queen_solve.py
@@ -1,5 +1,6 @@
     """
-    Attempte 1: 
+    ______________________________________________________________
+    Study 1: 
     Let's start with a 4x4 board as a concrete example.
         - Put a queen Q1 on a cell (1, 1) of the first row;
         - Determine, on the following row, all of the cells which are attached and avoid them.
@@ -15,8 +16,7 @@
     Should we generalize the solution's search to starting from the i-th row and then search for other rows ?
 
     ______________________________________________________________
-
-    Attempt 2:
+    Study 2:
     We notice that:
         - We can reduce the solving of a board by focusing on a quarter of a board, in fact, by placing a queen on a cell belonging 
         to the up-left quarter (and generating the consequent solutions, as described in the comment above), we can attend all
@@ -25,6 +25,16 @@
             + Symmetry to the horizontal axis of the board;
             + Symmetry on the origin: up-left to down-right;
             + Symmetry on the origin: down-left to up-right.
+
+    ______________________________________________________________
+    Study 3:
+    Keeping in mind the symmetries in play, we focus on the up-left quarter (ULQ) of the board with the following algorithm:
+        - Put a queen Q1 on the ULQ;
+        - Put a queen Q2 on a non-attacked cell on the first available "most-up-line and most-left-column" cell
+            + Put a queen Q3 on a non-attacked cell on the first available "most-up-line and most-left-column" cell
+                ++ recurse until no cells are non-attacked, if so, close the current branch and fold back to the immediate upper leaf
+                    of recursion, exhaust it.
+
 
     """
 
