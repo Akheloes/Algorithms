e9fee41ddaab13f5113e1404be133ddc11dac952
Akheloes <fayssaloudbib@gmail.com>
2020-09-10 20:21:34 +0200

    network class

--- installing_monitoring_devices_on_network/algorithm.py
+++ installing_monitoring_devices_on_network/algorithm.py
@@ -11,6 +11,16 @@ class Segment:
     def __str__(self):
         return '[ ' + self.first_end + ' , ' + self.second_end + ' ]' 
 
-s = Segment('A', 'B')
+class Network:
+    def __init__(self, segments):
+        self.segments = segments
 
-print(str(s))
+    def __str__(self):
+        network = ''
+        for segment in self.segments:
+            network += segment.__str__() + ' '
+        return network
+
+segments = [Segment('A', 'B'), Segment('A', 'C')]
+network = Network(segments)
+print(str(network))
