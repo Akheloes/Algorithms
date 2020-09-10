3ce092e3ee0df1bb21194280bf7a2ee340271fbf
Akheloes <fayssaloudbib@gmail.com>
2020-09-10 20:41:52 +0200

    adding node class, refactoring the representation-code

--- installing_monitoring_devices_on_network/algorithm.py
+++ installing_monitoring_devices_on_network/algorithm.py
@@ -1,15 +1,25 @@
 """
 Write the classes serving the representation of the problem. These classes are:
+    - Node;
     - Segment;
     - Network
 """
+# A node is characterized by its name
+class Node:
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return self.name
+
+# A segment is recognizable by two nodes
 class Segment:
     def __init__(self, first_end, second_end):
-        self.first_end = first_end
-        self.second_end = second_end
+        self.first_end = Node(first_end)
+        self.second_end = Node(second_end)
 
     def __str__(self):
-        return '[ ' + self.first_end + ' , ' + self.second_end + ' ]' 
+        return '[ ' + self.first_end.__str__() + ' , ' + self.second_end.__str__() + ' ]' 
 
 class Network:
     def __init__(self, segments):
@@ -21,6 +31,6 @@ class Network:
             network += segment.__str__() + ' '
         return network
 
-segments = [Segment('A', 'B'), Segment('A', 'C')]
+segments = [Segment('A','B'), Segment('A', 'C'), Segment('C', 'D')]
 network = Network(segments)
 print(str(network))
--- installing_monitoring_devices_on_network/readme.md
+++ installing_monitoring_devices_on_network/readme.md
@@ -6,6 +6,6 @@ Three modes of representation should be considered:
     - one where the network is composed solely of segments (each segment is defined by two ends/nodes);
     - an hybrid one, meaning one with both segments and nodes, it might seem redundant, but it can offer some facility.
 
-We will modelize the problem by resorting to the second mode of representation.
+We will modelize the problem by resorting to the third mode of representation.
 
 
