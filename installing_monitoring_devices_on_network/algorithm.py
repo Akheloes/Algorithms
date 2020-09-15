ae6cf8640d3daa1a2016328e7beb4f1297a03f94
Akheloes <fayssaloudbib@gmail.com>
2020-09-15 20:54:57 +0200

    enhancing the nodes extraction from a network set of segments

--- installing_monitoring_devices_on_network/algorithm.py
+++ installing_monitoring_devices_on_network/algorithm.py
@@ -14,12 +14,18 @@ class Node:
 
 # A segment is recognizable by two nodes
 class Segment:
+    index: int
     def __init__(self, first_end, second_end):
         self.first_end = Node(first_end)
         self.second_end = Node(second_end)
 
     def __str__(self):
         return '[ ' + self.first_end.__str__() + ' , ' + self.second_end.__str__() + ' ]' 
+
+    def nodeInSegment(self, node_name):
+        if self.first_end.name == node_name or self.first_end.name == node_name:
+            return True
+
 
 class Network:
     nodes = []
@@ -41,10 +47,22 @@ class Network:
                 names.append(segment.first_end.name)
             if segment.second_end.name not in names:
                 names.append(segment.second_end.name)
+        for name in names:
+            self.nodes.append(Node(name))
         return names
+
+    # Pick somes nodes such as all segments are covered
+    # There need not be optimization at this stage, a.k.a: we are not trying to minimize the set of needed nodes.
+    def monitoring_nodes(self):
+        self.extract_nodes()
+        # TODO: return all the segments which contains a given node
+            
+        
+
 
 segments = [Segment('A','B'), Segment('A', 'C'), Segment('C', 'D')]
 network = Network(segments)
-names = network.extract_nodes()
-for name in names:
-    print(name)
+# names = network.extract_nodes()
+# for name in names:
+#     print(name)
+network.monitoring_nodes()
