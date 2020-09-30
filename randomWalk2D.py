78e76eeee5003f928cc16970ca87fc76c41abcce
Akheloes <fayssaloudbib@gmail.com>
2020-09-30 20:30:53 +0200

    2D random walk enhanced

--- randomWalk2D.py
+++ randomWalk2D.py
@@ -4,6 +4,7 @@ def random_walk(n):
     x = 0
     y = 0
     for i in range(n):
+        """ returns a position after a random walk of n steps """
         direction = random.choice(['N', 'E', 'O', 'S'])
         if (direction == 'N'):
             y = y + 1
@@ -15,6 +16,17 @@ def random_walk(n):
             y = y - 1
     return (x, y)
 
+def random_walk_2D(n):
+    """ returns a position after a random walk of n steps """
+    x = 0
+    y = 0
+    for i in range(n):
+        (dx, dy) = random.choice([(1, 0), (0, 1), (-1, 0), (0, -1)])
+        x += dx
+        y += dy
+    return (x, y)
+
+# rw = random_walk(10)
 rw = random_walk(10)
 print('ten steps random walk: ', rw)
         
